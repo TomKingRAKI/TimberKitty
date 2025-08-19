8e96f2768402323064b7d8e7b31b4dbfbe9cbedb
TomKingRAKI <tomszma12@gmail.com>
2025-08-19 11:59:03 +0200

    dwa

--- frontend/script.js
+++ frontend/script.js
@@ -512,7 +512,7 @@ async function playLootboxAnimation(wonItem, boxData) {
         const itemData = shopData[loot.itemId];
         const itemDiv = document.createElement('div');
         itemDiv.className = `reel-item rarity-${loot.rarity}`;
-        itemDiv.innerHTML = `<div class="reel-item-content"><span>${itemData.icon}</span></div>`;
+        itemDiv.innerHTML = `<span>${itemData.icon}</span>`;
         animationReel.appendChild(itemDiv);
     });
 
